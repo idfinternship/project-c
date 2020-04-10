4984cb8e5d808e4a3071a97f2eac1677c1555d59
WeDDo <mantuxas001@gmail.com>
2020-04-10 13:12:55 +0300

    python file changes

--- ourproject/settings.py
+++ ourproject/settings.py
@@ -123,4 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-LOGIN_REDIRECT_URL ='/accounts/dashboard'
+LOGIN_REDIRECT_URL = '/accounts/dashboard'
+LOGOUT_URL = '/accounts/logout/'
+LOGOUT_REDIRECT_URL = '/'
