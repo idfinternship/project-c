fa2579dcfc16e15f05bb2d29398c56fc1a73081e
WeDDo <mantuxas001@gmail.com>
2020-04-10 18:42:13 +0300

    restored

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,14 @@
+from django.shortcuts import render,redirect
 from accounts.forms import RegistrationForm
 from django.contrib.auth.decorators import login_required
 
 
 def index(request):
+    return render(request, 'accounts/index.html')
 
 @login_required
 def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
 
 def register(request):
     if request.method == "POST":
@@ -15,4 +18,6 @@ def register(request):
             return redirect('login_url')
     else:
         form = RegistrationForm()
+
+    return render(request, 'accounts/register.html', {'form': form})
 
