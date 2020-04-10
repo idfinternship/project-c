69270bf4ffec1caa3b813300b32c63c8c5b84ef6
WeDDo <mantuxas001@gmail.com>
2020-04-10 13:11:38 +0300

    Accounts structure fix and front-end integration

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 
-app_name ='accounts'
+app_name = 'accounts'
 urlpatterns = [
-    path('', views.indexView, name="home"),
-    path('dashboard/', views.dashboardView, name="dashboard"),
-    path('register/', views.registerView, name="register_url"),
+    path('', views.index, name="index"),
+    path('dashboard/', views.dashboard, name="dashboard"),
+    path('register/', views.register, name="register_url"),
     path('login/', LoginView.as_view(), name="login_url"),
     path('logout/', LogoutView.as_view(next_page='dashboard'), name="logout"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,15 +2,15 @@ from django.shortcuts import render,redirect
 from accounts.forms import RegistrationForm
 from django.contrib.auth.decorators import login_required
 
-# Create your views here.
 
-def indexView(request):
-    return render(request,'registration/index.html')
+def index(request):
+    return render(request,'accounts/index.html')
 
 @login_required
-def dashboardView(request):
-    return render(request,'registration/dashboard.html')
-def registerView(request):
+def dashboard(request):
+    return render(request,'accounts/dashboard.html')
+
+def register(request):
     if request.method == "POST":
         form = RegistrationForm(request.POST)
         if form.is_valid():
@@ -19,4 +19,4 @@ def registerView(request):
     else:
         form = RegistrationForm()
 
-    return render(request,'registration/register.html', {'form':form})
+    return render(request,'accounts/register.html', {'form':form})
