0bba860b55699526e3da241db19f07c84fb82850
WeDDo <mantuxas001@gmail.com>
2020-04-16 13:01:35 +0300

    chat navbar and accounts fix

--- accounts/views.py
+++ accounts/views.py
@@ -15,7 +15,7 @@ def register(request):
         form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('login_url')
+            return redirect('accounts:login_url')
     else:
         form = RegistrationForm()
 
