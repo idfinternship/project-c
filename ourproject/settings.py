eff7d9038ef7cbc0c303df58b96a29af43fc76b1
Ricore <orentas.orentas20@gmail.com>
2020-04-29 19:25:40 +0300

    reset password

--- ourproject/settings.py
+++ ourproject/settings.py
@@ -129,3 +129,11 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/accounts/dashboard'
 LOGOUT_URL = '/accounts/logout/'
 LOGOUT_REDIRECT_URL = '/'
+
+#SMTP Configuration
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "foredge1@gmail.com"
+EMAIL_HOST_PASSWORD = "1G6YycI54IPa"
