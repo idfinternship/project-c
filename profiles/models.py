3af7afe2a5d1c8f2d4e6a782ad520984d9a0e61e
WeDDo <mantuxas001@gmail.com>
2020-04-30 11:01:44 +0300

    minor changes and pycache

--- profiles/models.py
+++ profiles/models.py
@@ -14,7 +14,6 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.username
 
-
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
