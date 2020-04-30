3856d9367cf532be5902cf4779c31a7bc73c8eec
NamelessSage <remyga39@gmail.com>
2020-04-30 13:08:49 +0300

    Code review

--- postfeed/views.py
+++ postfeed/views.py
@@ -1,12 +1,11 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
 from .models import Post
 from .forms import PostCreateForm
 
-from . import views
+
 @login_required
 def create(request):
     form = PostCreateForm(request.POST or None)
@@ -21,10 +20,12 @@ def create(request):
 
     return render(request, 'postfeed/create.html', context={'form': form})
 
+
 def delete_post(request, pk):
     post = Post.objects.get(pk=pk)
     post.delete()
     return redirect('postfeed:posts', username=request.user.username)
+
 
 @login_required
 def posts(request, username):
@@ -32,4 +33,3 @@ def posts(request, username):
     posts = searched_user.post.all().order_by('-creation_date')
 
     return render(request, 'postfeed/posts.html', context={'searched_user': searched_user, 'posts': posts})
-
--- profiles/models.py
+++ profiles/models.py
@@ -15,10 +15,12 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.username
 
+
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
+
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
