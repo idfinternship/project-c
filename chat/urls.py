c90b97af6313d92b81301a07908506e70c185190
Descory <simas0315@gmail.com>
2020-05-14 10:47:16 +0300

    Added chat design

--- chat/urls.py
+++ chat/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, re_path
 
-from .views import ThreadView, InboxView, friends_view
+from .views import ThreadView, InboxView, friends_view_inbox
 
 app_name = 'chat'
 urlpatterns = [
-    path("", friends_view, name='inbox'),
+    path("", friends_view_inbox, name='inbox'),
     re_path(r"^(?P<username>[\w.@+-]+)/$", ThreadView.as_view(), name='thread'),
 ]
